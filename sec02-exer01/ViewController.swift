495b148dd5bd383d74000d5944e4b5cefa967f4f
Brandon Ellis <Brandon_Ellis@Brandons-MacBook-Pro.local>
2015-12-23 00:55:27 -0600

    made 2nd app does some cool stuff complete unless add more features

--- sec02-exer01/ViewController.swift
+++ sec02-exer01/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var blueBall: UIImageView!
+    @IBOutlet var redBall: UIImageView!
+    @IBOutlet var hideRedButton: UIButton!
+    @IBOutlet var hideBlueButton: UIButton!
+    @IBOutlet var resetButton: UIButton!
+    @IBOutlet var showBlueButton: UIButton!
+    @IBOutlet var showRedButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func redBallHidden(sender: AnyObject) {
+        redBall.hidden = true
+        hideRedButton.hidden = true
+        showRedButton.hidden = false
+    }
+    
+    @IBAction func blueBallHidden(sender: AnyObject) {
+        blueBall.hidden = true
+        hideBlueButton.hidden = true
+        showBlueButton.hidden = false
+    }
+    @IBAction func resetImages(sender: AnyObject) {
+        redBall.hidden = false
+        blueBall.hidden = false
+    }
+    
+    @IBAction func showRedBall(sender: AnyObject) {
+        redBall.hidden = false
+        showRedButton.hidden = true
+        hideRedButton.hidden = false
+    }
+    @IBAction func showBlueBall(sender: AnyObject) {
+        blueBall.hidden = false
+        showBlueButton.hidden = true
+        hideBlueButton.hidden = false
+    }
+
 
 }
-
